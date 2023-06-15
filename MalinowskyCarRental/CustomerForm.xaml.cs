14caa6e4553b238a23a43ea36a176d51626ff5d9
Mateusz Malinowski <mateusz.malinowski.343@gmail.com>
2023-06-15 22:17:15 +0200

    Add CRUD operations support for clients table

---
+++ MalinowskyCarRental/CustomerForm.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System.Data.Entity.Migrations;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MalinowskyCarRental
+{
+    public partial class CustomerForm : Window
+    {
+        private readonly Klienci customer;
+        private readonly MalinowskyCarRentalEntities context;
+
+        public CustomerForm(in MalinowskyCarRentalEntities context, in Klienci customer = null)
+        {
+            InitializeComponent();
+            this.customer = customer;
+            this.context = context;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (customer != null)
+            {
+                header.Content = $"Edytuj klienta {customer.nazwisko}";
+                confirmButton.Text = "Edytuj";
+
+                pesel.Text = customer.pesel?.Trim();
+                imie.Text = customer.imie?.Trim();
+                nazwisko.Text = customer.nazwisko?.Trim();
+                data_urodzenia.SelectedDate = customer.data_urodzenia;
+                nr_telefonu.Text = customer.nr_telefonu?.Trim();
+                kraj.Text = customer.kraj?.Trim();
+                miasto.Text = customer.miasto?.Trim();
+                ulica.Text = customer.ulica?.Trim();
+                numer_domu.Text = customer.numer_domu?.Trim();
+                numer_lokalu.Text = customer.numer_lokalu?.Trim();
+
+                return;
+            }
+
+            header.Content = "Dodaj klienta";
+            confirmButton.Text = "Dodaj";
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left) DragMove();
+        }
+
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e) => Close();
+        private void ButtonConfirm_Click(object sender, RoutedEventArgs e)
+        {
+            Klienci newCustomer = new Klienci()
+            {
+                pesel = pesel.Text,
+                imie = imie.Text,
+                nazwisko = nazwisko.Text,
+                data_urodzenia = data_urodzenia.SelectedDate,
+                nr_telefonu = nr_telefonu.Text,
+                kraj = kraj.Text,
+                miasto = miasto.Text,
+                ulica = ulica.Text,
+                numer_domu = numer_domu.Text,
+                numer_lokalu = numer_lokalu.Text,
+            };
+
+            if (customer != null) newCustomer.id_klienta = customer.id_klienta;
+
+            context.Klienci.AddOrUpdate(newCustomer);
+            context.SaveChanges();
+
+            Close();
+        }
+    }
+}
--- MalinowskyCarRental/MainWindow.xaml.cs
+++ MalinowskyCarRental/MainWindow.xaml.cs
@@ -1,10 +1,17 @@
 ﻿using System.Windows;
+using System.Windows.Data;
 using System.Windows.Input;
+using System.Data.Entity;
+using System.Linq;
+using System;
 
 namespace MalinowskyCarRental
 {
     public partial class MainWindow : Window
     {
+        private MalinowskyCarRentalEntities context = new MalinowskyCarRentalEntities();
+        private CollectionViewSource klienciViewSource;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -19,14 +26,13 @@ namespace MalinowskyCarRental
         {
             if (e.ClickCount != 2) return;
 
-
             if (WindowState == WindowState.Maximized)
             {
                 WindowState = WindowState.Normal;
                 Width = 1280; Height = 720;
                 return;
             }
-            
+
             if (WindowState == WindowState.Normal)
             {
                 WindowState = WindowState.Maximized;
@@ -35,5 +41,53 @@ namespace MalinowskyCarRental
         }
 
         private void LogOutButton_Click(object sender, RoutedEventArgs e) => Close();
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            klienciViewSource = (CollectionViewSource)FindResource("klienciViewSource");
+            context.Klienci.Load();
+            context.Wypozyczenia.Load();
+            klienciViewSource.Source = context.Klienci.Local;
+        }
+
+        private void DeleteCommandHandler(object sender, ExecutedRoutedEventArgs e)
+        {
+            const string messageText = "Czy jesteś pewny? Wszystkie wypożyczenia tego klienta także zostaną usunięte.";
+            MessageBoxResult result = MessageBox.Show(messageText, "Ostrzeżenie",
+                MessageBoxButton.YesNoCancel, MessageBoxImage.Warning, MessageBoxResult.No);
+
+            if (result != MessageBoxResult.Yes) return;
+
+            Klienci current = klienciViewSource.View.CurrentItem as Klienci;
+
+            // delete all rentals related to this customer
+            foreach (Wypozyczenia rental in current.Wypozyczenia.ToList())
+            {
+                Wypozyczenia rentalToDelete = (from o in context.Wypozyczenia.Local
+                                               where o.id_wypozyczenia == rental.id_wypozyczenia
+                                               select o).FirstOrDefault();
+                context.Wypozyczenia.Remove(rentalToDelete);
+            }
+
+            context.Klienci.Remove(current);
+            context.SaveChanges();
+            klienciViewSource.View.Refresh();
+        }
+        
+        private void UpdateCommandHandler(object sender, ExecutedRoutedEventArgs e)
+        {
+            CustomerForm customerForm = new CustomerForm(context, klienciViewSource.View.CurrentItem as Klienci);
+            customerForm.Owner = this;
+            customerForm.ShowDialog();
+            klienciViewSource.View.Refresh();
+        }
+        
+        private void AddCommandHandler(object sender, ExecutedRoutedEventArgs e)
+        {
+            CustomerForm customerForm = new CustomerForm(context);
+            customerForm.Owner = this;
+            customerForm.ShowDialog();
+            klienciViewSource.View.Refresh();
+        }
     }
 }
